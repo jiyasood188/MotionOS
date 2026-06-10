244262adb4595ccc38c2d3e34fdfdf2e315e9bb3
jiya sood <jiyasood188@gmail.com>
2026-06-10 16:56:05 +0530

    new file added

---
+++ config.py
@@ -0,0 +1,11 @@
+camera_index=0
+detection_confidence=0.8
+tracking_confidence=0.7
+smoothing=5
+
+frame_reduction=0.6
+click_cooldown=0.4
+pinch_threshold=0.045
+scroll_speed=80
+volume_step=2
+volume_frames=6
