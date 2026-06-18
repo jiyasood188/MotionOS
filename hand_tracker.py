40e841f7730972d67c04ac8b7be2cbec7f0a98a6
Jiya <jiyasood188@gmail.com>
2026-06-18 10:41:20 +0530

    Refactor hand tracking to use HandLandmarker

--- hand_tracker.py
+++ hand_tracker.py
@@ -1,9 +1,24 @@
 import cv2
+
 import mediapipe as mp
+from mediapipe.tasks import python as mp_python
+from mediapipe.tasks.python import vision
 import config
+import urllib.request
+import os
 
 
-class hand_tracker:
+
+MODEL_PATH = "hand_landmarker.task"
+if not os.path.exists(MODEL_PATH):
+    print("Downloading hand landmark model...")
+    urllib.request.urlretrieve(
+        "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+        MODEL_PATH
+    )
+    print("Model downloaded.")
+
+class HandTracker:
     WRIST      = 0
     THUMB_TIP  = 4
     INDEX_TIP  = 8
@@ -16,38 +31,59 @@ class hand_tracker:
     PINKY_PIP  = 18
 
     def __init__(self):
-        self._mp_hands=mp.solutions.hands
-        self._hands=self._mp_hands.Hands(
-            max_num_hands=1,
-            min_detection_confidence=config.detection_confidence,
-            min_tracking_confidence=config.tracking_confidence
+         
+        base_options = mp_python.BaseOptions(model_asset_path=MODEL_PATH)
+
+        
+        options = vision.HandLandmarkerOptions(
+            base_options=base_options,
+            num_hands=1,
+            min_hand_detection_confidence=config.DETECTION_CONFIDENCE,
+            min_tracking_confidence=config.TRACKING_CONFIDENCE,
         )
+
+        
+        self._detector = vision.HandLandmarker.create_from_options(options)
+
+        
         self._draw = mp.solutions.drawing_utils
         self._draw_styles = mp.solutions.drawing_styles
+        self._mp_hands = mp.solutions.hands
 
-    def processing(self, frame):
+    def process(self, frame):
         h, w = frame.shape[:2]
+
+        
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        rgb.flags.writeable = False
-        results = self._hands.process(rgb)
-        rgb.flags.writeable = True
-        annotated = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
-        if not results.multi_hand_landmarks:
+
+        
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
+
+        result = self._detector.detect(mp_image)
+
+        annotated = frame.copy()
+
+        if not result.hand_landmarks:
             return None, None, annotated
-        hand = results.multi_hand_landmarks[0]
 
+         
+        hand = result.hand_landmarks[0]
 
-        self._draw.draw_landmarks(
-            annotated,
-            hand,
-            self._mp_hands.HAND_CONNECTIONS,
-            self._draw_styles.get_default_hand_landmarks_style(),
-            self._draw_styles.get_default_hand_connections_style()
-        )
-        landmarks_norm = [(lm.x, lm.y, lm.z) for lm in hand.landmark]
-        landmarks_px = [(int(lm.x * w), int(lm.y * h)) for lm in hand.landmark]
+      
+        landmarks_norm = [(lm.x, lm.y, lm.z) for lm in hand]
+
+       
+        landmarks_px = [(int(lm.x * w), int(lm.y * h)) for lm in hand]
+
+        for connection in self._mp_hands.HAND_CONNECTIONS:
+            start = landmarks_px[connection[0]]
+            end = landmarks_px[connection[1]]
+            cv2.line(annotated, start, end, (0, 200, 0), 2)
+
+        for point in landmarks_px:
+            cv2.circle(annotated, point, 4, (0, 255, 0), -1)
+
         return landmarks_norm, landmarks_px, annotated
 
     def close(self):
-        self._hands.close()
-
+        self._detector.close()
