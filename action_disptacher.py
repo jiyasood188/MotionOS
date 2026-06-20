f6dca73b2636ecf5a14f173d565fa723a02b3aa0
Jiya <jiyasood188@gmail.com>
2026-06-21 02:11:26 +0530

    Refactor gesture handling and add double click support

--- action_disptacher.py
+++ action_disptacher.py
@@ -1,14 +1,14 @@
-
 import time
 import pyautogui
 import comtypes
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from ctypes import cast, POINTER
 import config
-from guestre_classifier import (
+from gesture_classifier import (
     GESTURE_NONE,
     GESTURE_POINT,
     GESTURE_CLICK,
+    GESTURE_DOUBLE_CLICK,
     GESTURE_VOLUME_UP,
     GESTURE_VOLUME_DOWN,
     GESTURE_SCROLL_UP,
@@ -18,30 +18,37 @@ from guestre_classifier import (
     GESTURE_DISABLE,
 )
 
-
 comtypes.CoInitialize()
 _speakers = AudioUtilities.GetSpeakers()
 _interface = _speakers.Activate(IAudioEndpointVolume._iid_, comtypes.CLSCTX_ALL, None)
 _volume = cast(_interface, POINTER(IAudioEndpointVolume))
-_vol_min, _vol_max, _ = _volume.GetVolumeRange()
 
 _screen_w, _screen_h = pyautogui.size()
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = 0
 
-
 _last_click_time = 0
+_last_scroll_time = 0
+_last_volume_time = 0
+_last_media_time = 0
+_disable_counter = 0
 _control_enabled = True
 _smooth_x = 0
 _smooth_y = 0
 
 
 def dispatch(gesture, index_tip_norm):
-    global _last_click_time, _control_enabled, _smooth_x, _smooth_y
+    global _last_click_time, _last_scroll_time, _last_volume_time, _last_media_time, _disable_counter, _control_enabled, _smooth_x, _smooth_y
 
     if gesture == GESTURE_DISABLE:
-        _control_enabled = False
+        _disable_counter += 1
+        if _disable_counter >= config.DISABLE_FRAMES:
+            _control_enabled = False
+            _disable_counter = 0
         return _control_enabled
+
+    if gesture != GESTURE_DISABLE:
+        _disable_counter = 0
 
     if not _control_enabled:
         return _control_enabled
@@ -60,33 +67,55 @@ def dispatch(gesture, index_tip_norm):
             pyautogui.click()
             _last_click_time = current_time
 
+    elif gesture == GESTURE_DOUBLE_CLICK:
+        current_time = time.time()
+        if current_time - _last_click_time > config.CLICK_COOLDOWN:
+            pyautogui.doubleClick()
+            _last_click_time = current_time
+
     elif gesture == GESTURE_VOLUME_UP:
-        current_vol = _volume.GetMasterVolumeLevel()
-        new_vol = current_vol + (config.VOLUME_STEP / 100) * (_vol_max - _vol_min)
-        new_vol = min(new_vol, _vol_max)
-        _volume.SetMasterVolumeLevel(new_vol, None)
+        current_time = time.time()
+        if current_time - _last_volume_time > 0.3:
+            current_vol = _volume.GetMasterVolumeLevelScalar()
+            new_vol = min(current_vol + 0.02, 1.0)
+            _volume.SetMasterVolumeLevelScalar(new_vol, None)
+            _last_volume_time = current_time
 
     elif gesture == GESTURE_VOLUME_DOWN:
-        current_vol = _volume.GetMasterVolumeLevel()
-        new_vol = current_vol - (config.VOLUME_STEP / 100) * (_vol_max - _vol_min)
-        new_vol = max(new_vol, _vol_min)
-        _volume.SetMasterVolumeLevel(new_vol, None)
+        current_time = time.time()
+        if current_time - _last_volume_time > 0.3:
+            current_vol = _volume.GetMasterVolumeLevelScalar()
+            new_vol = max(current_vol - 0.02, 0.0)
+            _volume.SetMasterVolumeLevelScalar(new_vol, None)
+            _last_volume_time = current_time
 
     elif gesture == GESTURE_SCROLL_UP:
-        pyautogui.press('pageup')
+        current_time = time.time()
+        if current_time - _last_scroll_time > config.SCROLL_COOLDOWN:
+            pyautogui.press('pageup')
+            _last_scroll_time = current_time
 
     elif gesture == GESTURE_SCROLL_DOWN:
-        pyautogui.press('pagedown')
+        current_time = time.time()
+        if current_time - _last_scroll_time > config.SCROLL_COOLDOWN:
+            pyautogui.press('pagedown')
+            _last_scroll_time = current_time
 
     elif gesture == GESTURE_PAUSE:
-        pyautogui.press('space')
+        current_time = time.time()
+        if current_time - _last_media_time > 1.0:
+            pyautogui.press('playpause')
+            _last_media_time = current_time
 
     elif gesture == GESTURE_PLAY:
-        pyautogui.press('space')
+        current_time = time.time()
+        if current_time - _last_media_time > 1.0:
+            pyautogui.press('playpause')
+            _last_media_time = current_time
 
     return _control_enabled
 
 
 def enable_control():
     global _control_enabled
-    _control_enabled = True                
+    _control_enabled = True
